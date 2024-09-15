c38c7af8fdecbc029c7c598cb6ea3ff5320fa840
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-15 15:43:49 -0300

    adicionando classe LivroFavoritado nos models, passando a id do google, nota, notas_pessoais e tags

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+    
+class LivroFavoritado(models.Model):
+    usuario = models.ForeignKey(
+        get_user_model(),  
+        on_delete=models.CASCADE,
+        null=True
+    )
+    id_google = models.CharField(max_length=100, primary_key=True)
+    nota = models.IntegerField()
+    notas_pessoais = models.TextField()
+    tags = models.JSONField()
+
+    
+
+    def __str__ (self):
+        return "{} {} {}".format(self.id_google, self.nota, self.notas_pessoais, self.tags)

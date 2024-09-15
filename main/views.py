0cc604fcc0d93c15f4fd002b2c6633104c1922dd
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-15 15:42:37 -0300

    adicionando view da api e a classe LivroFavoritado e setando os fields como nota, notas pessoais e tags

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import LivroFavoritado
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# from django.urls import reverse_lazy
+ 
+
+class LivroFavoritadoViewSet(ModelViewSet):
+    model = LivroFavoritado
+    fields = [
+        'nota', 'notas_pessoais', 'tags'
+    ]
+
+    def get_queryset(self):
+        return LivroFavoritado.objects.filter(usuario=self.request.user)
+    
+    permission_classes = [IsAuthenticated]
+

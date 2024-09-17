1c4db0913144449d961ecb13869f6faaa30a87de
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-17 18:37:43 -0300

    ajustando settings do python e adicionaddo mais bibliotecas

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     "corsheaders",
+    'django_filters',
     'main', 
 ]
 
@@ -138,7 +139,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {

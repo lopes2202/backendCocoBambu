0cf12c3e204638e1ae5a01c92b0eeaeb23eff4fb
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-17 18:37:57 -0300

    ajustando models

--- main/models.py
+++ main/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
-
-
     
+class Tag(models.Model):
+    usuario = models.ForeignKey(
+        get_user_model(),  
+        on_delete=models.CASCADE,
+        null=True
+    )
+    nome = models.CharField(max_length=50)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['usuario', 'nome'], name='unique_tag_name_per_user')
+        ]
+
+
 class LivroFavoritado(models.Model):
     usuario = models.ForeignKey(
         get_user_model(),  
@@ -13,9 +25,10 @@ class LivroFavoritado(models.Model):
     id_google = models.CharField(max_length=100)
     nota = models.IntegerField()
     notas_pessoais = models.TextField()
-    tags = models.JSONField()
-
-    
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True
+    )
 
     def __str__ (self):
         return "{} {} {}".format(self.id_google, self.nota, self.notas_pessoais, self.tags)
